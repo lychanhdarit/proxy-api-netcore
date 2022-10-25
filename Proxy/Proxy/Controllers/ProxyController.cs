74a2091c1f33aac988b7d59cfe71f6bf941da87b
Ric Ly <ric.ly@seltec.io>
2022-10-25 16:47:54 +0700

    update

--- Proxy/Proxy/Controllers/ProxyController.cs
+++ Proxy/Proxy/Controllers/ProxyController.cs
@@ -17,7 +17,22 @@ namespace Proxy.Controllers
                     return Task.CompletedTask;
                 })
                 .Build();
+            //endpoint: full url
             return this.HttpProxyAsync($"{endpoint}?{queryString}", httpProxyOptions);
+        }
+        [Route("proxy2/{**endpoint}")]
+        public Task Proxy2(string endpoint)
+        {
+            var queryString = this.Request.QueryString.Value;
+            HttpProxyOptions httpProxyOptions = HttpProxyOptionsBuilder.Instance
+                .WithShouldAddForwardedHeaders(true)
+                .WithBeforeSend((c, hrm) =>
+                {
+                    return Task.CompletedTask;
+                })
+                .Build();
+            string host = "http://abc.com";//Custom  
+            return this.HttpProxyAsync($"{host}/{endpoint}?{queryString}", httpProxyOptions);
         }
     }
 }
